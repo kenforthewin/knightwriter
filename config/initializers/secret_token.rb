add8e37158790536986257930de2edb704ceaee9
Kenneth Bergquist <bergquist.kenneth@gmail.com>
2015-04-11 11:00:10 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Knightwriter::Application.config.secret_token = 'a46c1d73addbc4948ed9b96f5a4b56c5f424f72b5e1698c8c0db1093cd4b4a340afac80f627216ca267e4e65172ebef319257431652eeabf5e175fc0a2781887'
